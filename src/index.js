aedc91da227a28b1d63d40b612437ab8012e0290
Matthew <Remics89@gmail.com>
2022-10-19 23:52:46 +0000

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+
+const App = () => {
+    
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
