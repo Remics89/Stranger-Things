0e42c701f0053ccd0b4d265a3482da3810bd2b07
Matthew <Remics89@gmail.com>
2022-10-21 08:26:52 +0000

    feat: added working navigation to login/signup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
+import { fetchPosts, registerUser, userLogin } from "./api/api";
+import { Header, AuthorizeUser } from "./components";
 
 const App = () => {
-    
-}
+    const [Username, setUsername] = useState("");
+    const [Password, setPassword] = useState("");
+    const [Auth, setAuth] = useState(false);
 
-ReactDOM.render(<App />, document.getElementById('app'));
+    return (
+        <div className="app">
+            <Header />
+            <Switch>
+                <Route exact path="/Auth">
+                    <AuthorizeUser
+                        Link
+                        Auth={Auth}
+                        setAuth={setAuth}
+                        Password={Password}
+                        setPassword={setPassword}
+                        Username={Username}
+                        setUsername={setUsername}
+                    />
+                </Route>
+            </Switch>
+        </div>
+    );
+};
 
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById("container")
+);
