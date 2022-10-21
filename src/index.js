dd54b8c15606255cf89e502af7acb8f5a118201c
Matthew <Remics89@gmail.com>
2022-10-21 12:27:09 +0000

    refactor: simplified component and props, added state vars

--- src/index.js
+++ src/index.js
@@ -5,24 +5,20 @@ import { fetchPosts, registerUser, userLogin } from "./api/api";
 import { Header, AuthorizeUser } from "./components";
 
 const App = () => {
-    const [Username, setUsername] = useState("");
-    const [Password, setPassword] = useState("");
     const [Auth, setAuth] = useState(false);
+    const [Token, setToken] = useState(window.localStorage.getItem('token') | "");
+
+    const [AuthMode, setAuthMode] = useState("");
 
     return (
         <div className="app">
-            <Header />
+            <Header Auth={Auth} />
             <Switch>
-                <Route exact path="/Auth">
-                    <AuthorizeUser
-                        Link
-                        Auth={Auth}
-                        setAuth={setAuth}
-                        Password={Password}
-                        setPassword={setPassword}
-                        Username={Username}
-                        setUsername={setUsername}
-                    />
+                <Route exact path="/signup">
+                    <AuthorizeUser Auth={Auth} setAuth={setAuth} setToken={setToken}/>
+                </Route>
+                <Route exact path="/login">
+                    <AuthorizeUser Auth={Auth} setAuth={setAuth} setToken={setToken}/>
                 </Route>
             </Switch>
         </div>
