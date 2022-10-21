e609921fef85c0675bb4500fd589a4437f5e13d7
Matthew <Remics89@gmail.com>
2022-10-21 12:23:13 +0000

    feat: login/signup service

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,32 +1,148 @@
-import React, { useState } from 'react';
-
+import React, { useState, Fragment } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import { registerUser, userLogin } from "../api/api";
 
 const AuthorizeUser = (props) => {
-    const {Auth, setAuth, Password, setPassword, Username, setUsername} = props;
+    const { Auth, setAuth, setToken } = props;
 
+    /*
+    init state
+    */
 
+    const [Username, setUsername] = useState("");
+    const [Password, setPassword] = useState("");
+    const [Message, setMessage] = useState("");
+    const [Hidden, setHidden] = useState(false);
+    const [Success, setSuccess] = useState(false);
+
+    /* 
+    react modules
+    */
+
+    const { action } = useParams();
+    const history = useHistory();
+
+    const url = location.pathname;
+
+    /*
+    user registration
+    */
+
+    async function registration(user, pwd) {
+        try {
+            const data = await registerUser(user, pwd);
+            console.log(data.data.message);
+
+            if (data.success) {
+                setHidden(true);
+                setMessage(data.data.message);
+                setSuccess(true);
+
+                setTimeout(() => {
+                    history.push("/");
+                }, 7500);
+            } else {
+                setMessage(data.error.message);
+                setSuccess(false);
+                setTimeout(() => {
+                    setMessage("");
+                    setUsername("")
+                    setPassword("")
+                }, 5000);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    /*
+    user login 
+    */
+
+    async function authUser(user, pwd) {
+        try {
+            const data = await userLogin(user, pwd);
+
+            if (data.success) {
+                setMessage(
+                    data.data.message + " You are now being redirected back to the home page."
+                );
+                setHidden(true);
+                setAuth(true);
+                setSuccess(true);
+
+                setTimeout(() => {
+                    history.push("/");
+                }, 7500);
+            } else {
+                setMessage(data.error.message);
+                setSuccess(false);
+                setTimeout(() => {
+                    setMessage("");
+                    setUsername("")
+                    setPassword("")
+                }, 5000);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    /* 
+    render registration form
+    */
 
     return (
         <div id="auth">
-            <h3>{Auth ? 'Log In' : 'Sign Up'}</h3>
-            <form id='auth' onSubmit={async (event) => {
-                event.preventDefault();
-                setAuth(true);
-                console.log(Auth)
-            }}>
-                <input type="text" placeholder='Enter a username' value={Username} minLength='8' required onChange={(event) => {
-                    let inputVal = event.target.value;
-                    setUsername(inputVal);
-                }} />
-                <input type="password" placeholder='Enter a password' value={Password} minLength='8' required onChange={(event) => {
-                    let inputVal = event.target.value;
-                    setPassword(inputVal);
-                }} />
-                <button>{Auth ? 'Log In' : 'Sign Up'}</button>
-            </form>
+            <h3>{url === "/Login" ? "Log In" : "Sign Up"}</h3>
+            <form
+                id="auth"
+                onSubmit={async (event) => {
+                    event.preventDefault();
+                    if (url === "/Login") {
+                        authUser(Username, Password);
+                    } else {
+                        registration(Username, Password);
+                    }
+                }}>
+                <label>{Success ? null : 'Username'}</label>
+                <input
+                    className="formElms"
+                    type="text"
+                    placeholder="Enter username"
+                    value={Username}
+                    minLength="8"
+                    required
+                    hidden={Hidden}
+                    onChange={(event) => {
+                        let inputVal = event.target.value;
+                        setUsername(inputVal);
+                    }}
+                />
 
+                <label>{Success ? null : 'Password'}</label>
+                <input
+                    className="formElms"
+                    type="password"
+                    placeholder="Enter password"
+                    value={Password}
+                    minLength="8"
+                    required
+                    hidden={Hidden}
+                    onChange={(event) => {
+                        let inputVal = event.target.value;
+                        setPassword(inputVal);
+                    }}
+                />
+                <button className="formElms" type="submit" hidden={Hidden}>
+                    {url === "/Login" ? "Log In" : "Sign Up"}
+                </button>
+            </form>
+            <div className="authMessage" style={Success ? { color: 'Green'} : { color: 'Red'}}>
+                {Message ? Message : null}
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default AuthorizeUser;
