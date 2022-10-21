1d2478730ff8385484c181fb5d9f2230a9ba2e98
Matthew <Remics89@gmail.com>
2022-10-21 12:25:19 +0000

    refactor: href on login and signup changed

--- src/components/header.js
+++ src/components/header.js
@@ -7,8 +7,8 @@ const Header = () => {
             <header className='header'>Stranger's Things</header>
             <a className="links" href='/'>HOME</a>
             <a className="links" href='/Posts'>POSTS</a>
-            <a className="links" href='/Auth'>LOGIN</a>
-            <a className="links" href='/Auth'>SIGN UP</a>
+            <a className="links" href='/Login'>LOGIN</a>
+            <a className="links" href='/Signup'>SIGN UP</a>
         </div>
     )
 }
