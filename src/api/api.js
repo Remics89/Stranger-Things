089105b93840f6640b089dcd9219ca6d6c6fd6b0
Matthew <Remics89@gmail.com>
2022-10-27 10:32:52 +0000

    refactored register api call

--- src/api/api.js
+++ src/api/api.js
@@ -2,11 +2,9 @@ const BASEURL = "https://strangers-things.herokuapp.com/api/2207-FTB-ET-WEB-PT/"
 
 export async function fetchPosts() {
     try {
-        
         const response = await fetch(`${BASEURL}posts`);
         const data = await response.json();
-        return data;
-
+        return data.data;
     } catch (error) {
         console.error(error);
     }
@@ -27,14 +25,28 @@ export const registerUser = async (username, password) => {
             }),
         });
 
-        
         const result = await response.json();
         return result;
-
     } catch (error) {
         throw error;
     }
 };
+
+export async function getUser(token) {
+    try {
+        const response = await fetch(`${BASEURL}users/me`, {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+        });
+
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        throw error;
+    }
+}
 
 export async function createPost() {}
 
@@ -57,7 +69,6 @@ export const userLogin = async (username, password) => {
 
         const result = await response.json();
         return result;
-
     } catch (error) {
         throw error;
     }
