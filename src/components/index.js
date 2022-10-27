8e402c8fdda19d3623172be611d3f7f6331736d8
Matthew <Remics89@gmail.com>
2022-10-27 10:35:57 +0000

    added new exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,6 @@
-export { default as Header } from "./header"
-export { default as AuthorizeUser } from "./auth"
+export { default as AuthorizeUser } from "./auth";
+export { default as Posts } from "./posts";
+export { default as Home } from "./Home";
+export { default as Footer } from "./footer";
+export { default as Post } from "./post";
+export { default as Dashboard } from "./account"
