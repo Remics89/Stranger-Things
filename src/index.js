c670a6d6df56b9f023e9eb4a48b8972596d8e305
Matthew <Remics89@gmail.com>
2022-10-27 10:27:05 +0000

    refactor of state, Routes and Links added for login, signup, posts

--- src/index.js
+++ src/index.js
@@ -1,26 +1,106 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
-import { fetchPosts, registerUser, userLogin } from "./api/api";
-import { Header, AuthorizeUser } from "./components";
+import { BrowserRouter as Router, Switch, Link, Route, useHistory } from "react-router-dom";
+import { fetchPosts, getUser } from "./api/api";
+import { AuthorizeUser, Posts, Footer, Home, Post, Dashboard } from "./components";
 
 const App = () => {
-    const [Auth, setAuth] = useState(false);
-    const [Token, setToken] = useState(window.localStorage.getItem('token') | "");
+    const [currentUser, setCurrentUser] = useState("");
+    const [Token, setToken] = useState(window.localStorage.getItem("token") || "");
+    const [posts, setPosts] = useState([]);
+    const [currentPost, setcurrentPost] = useState({});
 
-    const [AuthMode, setAuthMode] = useState("");
+    const history = useHistory();
+
+    useEffect(() => {
+        const getPosts = async () => {
+            try {
+                const result = await fetchPosts();
+                console.log("🚀 ~ getPosts ~ result", result);
+                setPosts(result.posts);
+            } catch (error) {
+                throw error;
+            }
+        };
+        getPosts();
+    }, []);
+
+    useEffect(() => {
+        const user = async () => {
+            try {
+                const result = await getUser(Token);
+                return result;
+            } catch (error) {
+                throw error;
+            }
+        };
+        user();
+    }, [Token]);
+
+    useEffect(() => {
+        if (Token) {
+            window.localStorage.setItem("token", Token);
+        } else {
+            window.localStorage.removeItem("token");
+        }
+    }, [Token]);
+
+    const logOut = () => {
+        setToken("");
+        history.push('/')
+    };
 
     return (
         <div className="app">
-            <Header Auth={Auth} />
+            <div className="head">
+                <header className="title">Stranger's Things</header>
+                <div className="ui menu">
+                    <Link className="item" to="/">
+                        Home
+                    </Link>
+                    <Link className="item" to="/Posts">
+                        Posts
+                    </Link>
+                    {Token ? (
+                        <Link className="item" to="/Account/Dash">
+                            Account
+                        </Link>
+                    ) : null}
+                    {Token ? (
+                        <Link className="item" to="/" onClick={logOut}>
+                            Log Out
+                        </Link>
+                    ) : (
+                        <Link className="item" to="/Account/Login">
+                            Log In
+                        </Link>
+                    )}
+                    {Token ? null : (
+                        <Link className="item" to="/Account/register">
+                            Sign Up
+                        </Link>
+                    )}
+                </div>
+            </div>
+
             <Switch>
-                <Route exact path="/signup">
-                    <AuthorizeUser Auth={Auth} setAuth={setAuth} setToken={setToken}/>
+                <Route exact path="/">
+                    <Home Token={Token} />
                 </Route>
-                <Route exact path="/login">
-                    <AuthorizeUser Auth={Auth} setAuth={setAuth} setToken={setToken}/>
+                <Route exact path="/Posts">
+                    <Posts Token={Token} posts={posts} setcurrentPost={setcurrentPost} />
+                </Route>
+                <Route exact path="/Account/:action">
+                    <AuthorizeUser setToken={setToken} setCurrentUser={setCurrentUser} />
+                </Route>
+                <Route path="/Posts/post">
+                    <Post currentPost={currentPost} currentUser={currentUser} Token={Token} />
+                </Route>
+                <Route exact path="/Account/Dash">
+                    <Dashboard />
                 </Route>
             </Switch>
+            <Footer />
         </div>
     );
 };
