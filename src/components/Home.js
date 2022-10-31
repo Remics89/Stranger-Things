b63e86382ae295d1f2e2d439098a18cb8361e6bd
Matthew <Remics89@gmail.com>
2022-10-31 06:04:49 +0000

    feat: built deletePost, conditionals, and state to track and deliver authenticated user content

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,102 +1,205 @@
 import React, { useState, useEffect } from "react";
-import { fetchPosts } from "../api/api";
+import { createPost, deletePost } from "../api/api";
 import redX from "./ico/RedX.png";
 import greenCheck from "./ico/GreenCheck.png";
 
-const Home = ({ Token, posts, currentUser }) => {
-    const [render, setrender] = useState(false);
-    const [hide, sethide] = useState(false);
-    const [style, setstyle] = useState({});
 
-    const [myPosts, setmyPosts] = useState();
+/* main Home component */
 
-    const authoredPosts = () => {};
+const Home = ({ Token, currentUser, posts }) => {
+    const [description, setdescription] = useState("");
+    const [title, setTitle] = useState("");
+    const [price, setprice] = useState("$");
+    const [location, setlocation] = useState("");
+    const [deliver, setdeliver] = useState(false);
 
-    useEffect(() => {
-        authoredPosts();
-        setrender(true);
-        setInterval(() => {
-            // hide Loading... 
-            sethide(true);
-            // render in a overflow scroll 
-            if (render) {
-                setstyle({ overflow: "scroll" });
+
+    /* helper function for home component returning mapped out posts for which logged in user is creator */
+
+    const mapMyPosts = () => {
+        return myPosts.length > 0 ? (
+            myPosts.map((post) => {
+                let dateString = post.createdAt.slice(0, 10);
+                return (
+                    <div key={post._id} className="ui centered card" style={{border: "1px dashed black"}}>
+                        <div className="content">
+                            <div className="header">{post.title}</div>
+                            <div className="meta">Created: {dateString}</div>
+                            <div className="meta">Price: {post.price}</div>
+                            <div className="meta">
+                                Post Active:
+                                {post.active ? (
+                                    <img className="willDeliver" src={greenCheck} />
+                                ) : (
+                                    <img className="willDeliver" src={redX} />
+                                )}
+                            </div>
+                        </div>
+                        <DeletePost currentUser={currentUser} postID={post._id} Token={Token} />
+                    </div>
+                );
+            })
+        ) : (
+            <h4>You have not created any posts yet</h4>
+        );
+    };
+
+
+    /* helper function for main dashboard component that provides delete post functionality */
+
+    const DeletePost = ({ currentUser, postID, Token }) => {
+        const deleteThisPost = async (Token, postID) => {
+
+            try {
+                const result = await deletePost(Token, postID);
+                if (result.success) {
+                    return "Post Deleted";
+                } else {
+                    return "Post failed to delete";
+                }
+            } catch (error) {
+                throw error;
             }
-        }, 2000);
-    }, [posts]);
+        };
 
-    return (
-        <div className="ui placeholder segment">
-            <div className="ui stackable very relaxed two column grid">
-                <div className="column">
-                    <div className="field" id="homecards" style={style}>
-                        <h2 hidden={hide}>Loading Posts ...</h2>
-                        {render ? (
-                            posts.map((post) => {
-                                let dateString = post.createdAt.slice(0, 10);
-                                return (
-                                    <div key={post._id} className="ui centered card">
-                                        <div className="content">
-                                            <div className="header">{post.title}</div>
-                                            <div className="meta">Created: {dateString}</div>
-                                            <div className="meta">Price: {post.price}</div>
-                                            <div className="meta">
-                                                Post Active:
-                                                {post.active ? (
-                                                    <img className="willDeliver" src={greenCheck} />
-                                                ) : (
-                                                    <img className="willDeliver" src={redX} />
-                                                )}
-                                            </div>
+        return (
+            <>
+                {currentUser.success ? (
+                    <button
+                        className="ui negative button"
+                        onClick={() => deleteThisPost(Token, postID)}>
+                        Delete Post
+                    </button>
+                ) : null}
+            </>
+        );
+    };
+
+
+
+
+    let myPosts = [];
+
+
+    /* conditional for loop grabbing ONLY posts for which logged in user is creator */
+
+    if (currentUser) {
+        if (currentUser.success) {
+            for (let key in posts) {
+                let post = posts[key];
+                let author = post.author.username;
+                if (currentUser.data.username === author) {
+                    myPosts.push(post);
+                }
+            }
+        }
+    }
+
+
+    /* onSubmit function to send API data for creating a new post */
+
+    const handleNewPostSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const result = await createPost(Token, title, price, location, description, deliver);
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    };
+
+
+    /* return statement for primary Home component with 2 conditional checks to: prevent an error, and to check if someone is logged in */
+
+    if (currentUser) {
+        if (currentUser.success) {
+            return (
+                <div className="ui placeholder segment">
+                    <div className="ui stackable very relaxed two column grid" id="home-centered">
+                        <div className="column">
+                            <div className="field" id="homecards" style={{ overflow: "auto" }}>
+                                {mapMyPosts()}
+                            </div>
+                        </div>
+                        <div className="middle aligned column">
+                            <form className="ui form" onSubmit={handleNewPostSubmit}>
+                                <div className="field">
+                                    <label>Title</label>
+                                    <div className="ui input">
+                                        <input
+                                            type="text"
+                                            placeholder="Title"
+                                            value={title}
+                                            onChange={(event) => setTitle(event.target.value)}
+                                        />
+                                    </div>
+                                </div>
+                                <div className="field">
+                                    <div className="field">
+                                        <label>Location</label>
+                                        <div className="ui input">
+                                            <input
+                                                type="text"
+                                                placeholder="Location"
+                                                value={location}
+                                                onChange={(event) =>
+                                                    setlocation(event.target.value)
+                                                }
+                                            />
                                         </div>
                                     </div>
-                                );
-                            })
-                        ) : (
-                            <h4>You have not created any posts yet</h4>
-                        )}
-                        <div className="middle aligned column"></div>
+                                    <div className="field">
+                                        <label>Price</label>
+                                        <div className="ui input">
+                                            <input
+                                                type="text"
+                                                placeholder="Price"
+                                                rows="3"
+                                                value={price}
+                                                onChange={(event) => setprice(event.target.value)}
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="field">
+                                        <label htmlFor="form-textarea-control-opinion">
+                                            Description
+                                        </label>
+                                        <textarea
+                                            placeholder="Description"
+                                            id="form-textarea-control-opinion"
+                                            rows="5"
+                                            value={description}
+                                            onChange={(event) =>
+                                                setdescription(event.target.value)
+                                            }></textarea>
+                                    </div>
+                                    <div className="ui checkbox">
+                                        <label>Will Deliver?</label>
+                                        <input
+                                            type="checkbox"
+                                            name="deliver"
+                                            value={deliver}
+                                            onChange={(event) => setdeliver(event.target.value)}
+                                        />
+                                    </div>
+                                </div>
+
+                                <button className="ui button" type="submit">
+                                    Submit
+                                </button>
+                            </form>
+                        </div>
                     </div>
+                    <div className="ui vertical divider">-- Your Posts | New Posts ---</div>
                 </div>
-            </div>
-            <div className="ui vertical divider">----- Your Posts | New Post -------</div>
-        </div>
-    );
+            );
+        } else {
+            return null;
+        }
+    } else {
+        return null;
+    }
 };
 
 export default Home;
-
-/*
-
-<div class="ui placeholder segment">
-    <div class="ui stackable very relaxed two column grid">
-        <div class="column">
-            <form class="ui form">
-                <div class="field">
-                    <label>Username</label>
-                    <div class="ui left icon input">
-                        <input type="text" placeholder="Username" />
-                        <i aria-hidden="true" class="user icon"></i>
-                    </div>
-                </div>
-                <div class="field">
-                    <label>Password</label>
-                    <div class="ui left icon input">
-                        <input type="password" />
-                        <i aria-hidden="true" class="lock icon"></i>
-                    </div>
-                </div>
-                <button class="ui primary button">Login</button>
-            </form>
-        </div>
-        <div class="middle aligned column">
-            <button class="ui big button">
-                <i aria-hidden="true" class="signup icon"></i>Sign up
-            </button>
-        </div>
-    </div>
-    <div class="ui vertical divider">Or</div>
-</div>;
-
-
-*/
